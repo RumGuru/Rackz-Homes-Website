646f0da5a81bda6fcad698af89ab4b1f2958afb7
Rahul Kothwal <rahulkothwal@Rahuls-MacBook-Pro.local>
2021-06-26 21:27:47 -0400

    test

--- mailer-new.php
+++ mailer-new.php
@@ -13,17 +13,17 @@
         exit;
     }
 
-    // Set the recipient email address. Update this to YOUR desired email address.
+    // Set the recipient email address.
     $recipient = "rahul11@hotmail.ca";
 
     // Set the email subject.
     $subject = "New contact from $name";
 
     // Build the email content.
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
-
+    //$email_content = "Name: $name\n";
+    //$email_content .= "Email: $email\n\n";
+    //$email_content .= "Message:\n$message\n";
+    $email_content = "test";
     // Build the email headers.
     $email_headers = "From: $name <$email>";
 
